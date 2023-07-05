abd8fa9627a618c33856a7d20d47f8bdcbaa0bf0
Attraya Das <attrayaghoshdas@gmail.com>
2023-07-05 15:57:16 +0530

    Create DTO Class - PostDto

--- src/main/java/com/attraya/blog/exception/ResourceNotFoundException.java
+++ src/main/java/com/attraya/blog/exception/ResourceNotFoundException.java
@@ -2,6 +2,7 @@ package com.attraya.blog.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
     private String resourceName;
