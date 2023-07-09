0c7f2c38e4eaf641d6d0335481154618e4db5440
Attraya Das <attrayaghoshdas@gmail.com>
2023-07-09 22:55:58 +0530

    Enable REST API Validation

--- src/main/java/com/attraya/blog/payload/PostDto.java
+++ src/main/java/com/attraya/blog/payload/PostDto.java
@@ -1,5 +1,7 @@
 package com.attraya.blog.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Set;
@@ -7,8 +9,21 @@ import java.util.Set;
 @Data
 public class PostDto {
     private Long id;
+
+    // title should not be empty or null
+    // title should have at least 2 characters
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters")
     private String title;
+
+    // post description should not be empty or null
+    // post description should have at least 10 characters
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 2 characters")
     private String description;
+
+    // post content should not be empty or null
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 }
