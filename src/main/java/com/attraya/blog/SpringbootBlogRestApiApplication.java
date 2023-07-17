662b3008141e854cd60c4d67b958c46596eaa98e
Attraya Das <attrayaghoshdas@gmail.com>
2023-07-18 00:13:05 +0530

    Implement Swagger Documentation

--- src/main/java/com/attraya/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/attraya/blog/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,35 @@
 package com.attraya.blog;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot Blog App REST APIs",
+				description = "Spring Boot Blog App REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Attraya",
+						email = "attrayaghoshdas@gmail.com",
+						url = "https://www.linkedin.com/in/attrayadas/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/attrayadas"
+				)
+		), externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Blog App Documentation",
+				url = "https://github.com/attrayadas/real-time-blog-application-springrest"
+        )
+) // go to: http://localhost:8080/swagger-ui/index.html
 public class SpringbootBlogRestApiApplication {
 
 	@Bean
--- src/main/java/com/attraya/blog/controller/PostController.java
+++ src/main/java/com/attraya/blog/controller/PostController.java
@@ -4,6 +4,10 @@ import com.attraya.blog.payload.PostDto;
 import com.attraya.blog.payload.PostResponse;
 import com.attraya.blog.service.PostService;
 import com.attraya.blog.utils.AppConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/posts")
+@Tag(name = "CRUD REST APIs for Post Resource")
 public class PostController {
 
     private PostService postService;
@@ -22,6 +27,17 @@ public class PostController {
         this.postService = postService;
     }
 
+    @Operation(
+            summary = "Create Post REST API",
+            description = "Create Post REST API is used to save post into database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
     // create blog post rest api
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
@@ -29,6 +45,14 @@ public class PostController {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Get all Posts REST API",
+            description = "Get all Posts REST API is used to fetch all the posts from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     // get all posts rest api
     @GetMapping
     public PostResponse getAllPosts(@RequestParam(value="pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
@@ -38,12 +62,31 @@ public class PostController {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
+    @Operation(
+            summary = "Get Post by Id REST API",
+            description = "Get Post by Id REST API is used to get single post from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     // get post by id rest api
     @GetMapping("/{id}")
     public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id){
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
+    @Operation(
+            summary = "Update Post REST API",
+            description = "Update Post REST API is used to update a particular post in the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
     // update post by id rest api
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
@@ -52,6 +95,17 @@ public class PostController {
         return new ResponseEntity<>(postResponse, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Delete Post REST API",
+            description = "Delete Post REST API is used delete a particular post from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
     // delete post rest api
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
