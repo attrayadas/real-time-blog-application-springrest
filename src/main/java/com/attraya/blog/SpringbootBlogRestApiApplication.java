97bad6647dfe95a8002e470b1f998eccc7bb4276
Attraya Das <attrayaghoshdas@gmail.com>
2023-07-20 00:50:14 +0530

    Add code to insert Metadata in Tables

--- src/main/java/com/attraya/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/attraya/blog/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,15 @@
 package com.attraya.blog;
 
+import com.attraya.blog.entity.Role;
+import com.attraya.blog.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -30,7 +34,7 @@ import org.springframework.context.annotation.Bean;
 				url = "https://github.com/attrayadas/real-time-blog-application-springrest"
         )
 ) // go to: http://localhost:8080/swagger-ui/index.html
-public class SpringbootBlogRestApiApplication {
+public class SpringbootBlogRestApiApplication implements CommandLineRunner {
 
 	@Bean
 	public ModelMapper modelMapper(){
@@ -41,4 +45,18 @@ public class SpringbootBlogRestApiApplication {
 		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
 	}
 
+	@Autowired
+	private RoleRepository roleRepository;
+	@Override
+	public void run(String... args) throws Exception {
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+	}
 }
